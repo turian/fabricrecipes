2ecb6ba9f6482c805ba737198bca08f4da8b9abc
Joseph Turian <turian@gmail.com>
2011-01-09 16:26:37 -0500

    Added fabricrecipes.ubuntu.*ubuntuversion()

--- fabricrecipes/ubuntu.py
+++ fabricrecipes/ubuntu.py
@@ -3,6 +3,14 @@ Ubuntu fabric recipes.
 """
 from fabric.api import *
 
+import string
+
 def aptdistupgrade():
     run('sudo apt-get update')
     run('sudo apt-get dist-upgrade -y')
+
+def ubuntuversion():
+    return string.strip(run('lsb_release -sr'))
+
+def mongoubuntuversion():
+    return ubuntuversion().replace(".0", ".")
