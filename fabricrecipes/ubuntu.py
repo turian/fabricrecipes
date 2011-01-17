139ecb7dd334165573e26eb44b8921df89e3c98b
Joseph Turian <turian@gmail.com>
2011-01-16 19:57:55 -0500

    Added fabricrecipes.ubuntu.enable_multiverse()

--- fabricrecipes/ubuntu.py
+++ fabricrecipes/ubuntu.py
@@ -6,11 +6,21 @@ from fabric.api import *
 import string
 
 def aptdistupgrade():
-    run('sudo apt-get update')
-    run('sudo apt-get dist-upgrade -y')
+    sudo('apt-get update')
+    sudo('apt-get dist-upgrade -y')
 
 def ubuntuversion():
     return string.strip(run('lsb_release -sr'))
+
+def ubuntucodename():
+    return string.strip(run('lsb_release -sc'))
+
+def enable_multiverse():
+    UBUNTUCODENAME = ubuntucodename()
+    sudo('echo "deb http://us.archive.ubuntu.com/ubuntu/ %s multiverse" >> /etc/apt/sources.list' % UBUNTUCODENAME)
+    sudo('echo "deb-src http://us.archive.ubuntu.com/ubuntu/ %s multiverse" >> /etc/apt/sources.list' % UBUNTUCODENAME)
+    sudo('echo "deb http://us.archive.ubuntu.com/ubuntu/ %s-updates multiverse" >> /etc/apt/sources.list' % UBUNTUCODENAME)
+    sudo('echo "deb-src http://us.archive.ubuntu.com/ubuntu/ %s-updates multiverse" >> /etc/apt/sources.list' % UBUNTUCODENAME)
 
 def mongoubuntuversion():
     return ubuntuversion().replace(".0", ".")
